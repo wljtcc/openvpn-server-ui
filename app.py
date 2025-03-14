902099cb2381dbce2b7550c4aa46effcd28d93bc
wljtc <wljtcc@gmail.com>
2025-03-14 11:51:17 -0300

    OpenVPN Server UI - change host

--- app.py
+++ app.py
@@ -49,4 +49,6 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,
+            port=5000,
+            host='0.0.0.0')
